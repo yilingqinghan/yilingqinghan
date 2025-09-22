ab54dd14fea50187b33e7857c064946934437d1f
yilingqinghan <759569457@qq.com>
2025-09-22 08:37:03 +0800

    其他资源
    Signed-off-by: yilingqinghan <759569457@qq.com>

---
+++ i18n.js
@@ -0,0 +1,193 @@
+const resources = {
+        en: {
+            translation: {
+                "name": "Yang Zi",
+                "tagline": "Master Degree Candidate",
+                "loc": "Wu Xi, Jiang Su",
+                "introduction": "You can make a PDF version of your\n" +
+                    "resume using our free Sketch template here. Donec quam felis, ultricies nec, pellentesque eu. Lorem\n" +
+                    "ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum\n" +
+                    "sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,\n" +
+                    "ultricies nec, pellentesque eu, pretium quis, sem. Maecenas nec odio et ante tincidunt tempus. Donec vitae\n" +
+                    "sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt.\n" +
+                    "Duis leo. Sed fringilla mauris sit amet nibh.",
+                "skills": "Skills",
+                "tech": "Technical",
+                "pro": "Professional",
+                "pros": "<ul class=\"list-unstyled resume-skills-list\">\n" +
+                    "<li class=\"mb-1\">Perfectionist</li>\n" +
+                    "<li class=\"mb-1\">Excellent leadership skills</li>\n" +
+                    "<li class=\"mb-1\">Outstanding Spoken English</li>\n" +
+                    "<li class=\"mb-1\">Strong time management</li>\n" +
+                    "<li class=\"mb-1\">Empathetic communicator</li>\n" +
+                    "<li class=\"mb-1\">Self-motivation and ambition</li>\n" +
+                    "<li class=\"mb-1\">Strong analytical skills</li>\n" +
+                    "<li>Detail-oriented</li>\n" +
+                    "</ul>",
+                "edu": "Education",
+                "education": "<li class=\"mb-3\">\n" +
+                    "<div class=\"resume-degree font-weight-bold\">BEng in Computer Science · 2019 - 2023</div>\n" +
+                    "<div class=\"resume-degree-org fw-bold fs-6\">University of Jiangsu</div>\n" +
+                    "<div class=\"resume-degree-org text-muted\">School of Computer Science and Communication Engineering</div>\n" +
+                    "</li>\n" +
+                    "<li class=\"mb-3\">\n" +
+                    "<div class=\"resume-degree font-weight-bold\">MEng in Artificial Intelligence · 2023 - 2026</div>\n" +
+                    "<div class=\"resume-degree-org fw-bold fs-6\">Jiangnan University</div>\n" +
+                    "<div class=\"resume-degree-org text-muted\">School of Artificial Intelligence and Computer Science</div>\n" +
+                    "</li>\n" +
+                    "<li>\n" +
+                    "<div class=\"resume-degree font-weight-bold\">Visiting Master's Student · 2024 - 2026</div>\n" +
+                    "<div class=\"resume-degree-org fw-bold fs-6\">Chinese Academy of Sciences</div>\n" +
+                    "<div class=\"resume-degree-org text-muted\">SKLP, ICT</div>\n" +
+                    "</li>",
+                "awa": "Awards",
+                "awards": "<li>\n" +
+                    "<div class=\"font-weight-bold fs-6\">🚩During Undergraduate</div>\n" +
+                    "</li>\n" +
+                    "<li class=\"mb-3\">\n" +
+                    "<div class=\"text-muted mb-1\">Outstanding Student Award(school)(2020 - 2022)</div>\n" +
+                    "<div class=\"text-muted\">Outstanding Graduate</div>\n" +
+                    "</li>\n" +
+                    "<li>\n" +
+                    "<div class=\"font-weight-bold fs-6\">🚩During Postgraduate</div>\n" +
+                    "</li>\n" +
+                    "<li>\n" +
+                    "<div class=\"text-muted mb-1\">[Leader🚩]Second Prize/National Champion, 2024 National College Student Computer System Competency Competition - Compiler System Challenge(Bisheng Cup)</div>\n" +
+                    "<div class=\"text-muted mb-1\">Third Prize, 2024 National College Student Computer System Competency Competition - Intelligent Computing Innovation Design Contest (Pioneer Cup)</div>\n" +
+                    "<div class=\"text-muted mb-1\">Outstanding Officer, Graduate Student Union</div>\n" +
+                    "<div class=\"text-muted mb-1\">Ranked 1st in Comprehensive Evaluation (1/72)</div>\n" +
+                    "<div class=\"text-muted mb-1\">First-Class Academic Scholarship (Top 5%)</div>\n" +
+                    "<div class=\"text-muted mb-1\">Outstanding Graduate Student Leader (22 awardees in the school)</div>\n" +
+                    "<div class=\"text-muted\">Xiaomi Scholarship Recipient (11 awardees in the school)</div>\n" +
+                    "</li>",
+                "lang": "Languages",
+                "langs": "<li class=\"mb-1\">Chinese <span class=\"text-muted\">(Native)</span></li>\n" +
+                    "<li class=\"mb-1\">English <span class=\"text-muted\">(Professional)</span></li>\n" +
+                    "<li class=\"mb-1\">CET4: 601</li><li>CET6: 550</li>",
+                "int": "Interests",
+                "sw": "Social Work",
+                "sws": "<li class=\"mb-1\">Class President, Master’s Program in Artificial Intelligence, Class of 2023</li>\n" +
+                    "<li class=\"mb-1\">Office Member, Graduate Student Union, School of Artificial Intelligence, Jiangnan University (2023-2024)</li>\n" +
+                    "<li class=\"mb-1\">Office Member, Graduate Student Union, Jiangnan University (University Level) (2023-2024)</li>\n" +
+                    "<li>70 Hours of Volunteer Service (2023-2024)</li>",
+                "inter": "<li class=\"mb-1\">Basketball</li>\n" +
+                    "<li class=\"mb-1\">Badminton</li>\n" +
+                    "<li class=\"mb-1\">sprint</li>\n" +
+                    "<li>swimming</li>",
+                "we":"Internship Experiences",
+                "proj": "Projects",
+                "pp": "Papers",
+                "ri": "Research Interests"
+            }
+        },
+        zh: {
+            translation: {
+                "name": "杨梓",
+                "tagline": "硕士研究生",
+                "loc": "江苏无锡",
+                "introduction": "",
+                "skills": "技能",
+                "tech": "技术型",
+                "pro": "专家型",
+                "pros": "<ul class=\"column\">\n" +
+                    "        <li>完美主义者</li>\n" +
+                    "        <li>优秀的领导能力</li>\n" +
+                    "        <li>出色的英语口语</li>\n" +
+                    "        <li>强大的时间管理能力</li>\n" +
+                    "    </ul>\n" +
+                    "    <ul class=\"column\">\n" +
+                    "        <li>善于沟通和共情</li>\n" +
+                    "        <li>自我驱动力与抱负</li>\n" +
+                    "        <li>卓越的分析能力</li>\n" +
+                    "        <li>注重细节</li>\n" +
+                    "    </ul>",
+                "edu":
+                    "教育经历",
+                "education":
+                    "<li class=\"mb-3\">\n" +
+                    "<div class=\"resume-degree font-weight-bold\">计算机科学与技术学士 · 2019 - 2023</div>\n" +
+                    "<div class=\"resume-degree-org fw-bold fs-6\">江苏大学</div>\n" +
+                    "<div class=\"resume-degree-org text-muted\">计算机科学与通信工程学院</div>\n" +
+                    "<div class=\"resume-degree-time text-muted\">2019 - 2023</div>\n" +
+                    "</li>\n" +
+                    "<li class=\"mb-3\">\n" +
+                    "<div class=\"resume-degree font-weight-bold\">人工智能硕士 · 2023 - 2026</div>\n" +
+                    "<div class=\"resume-degree-org fw-bold fs-6\">江南大学</div>\n" +
+                    "<div class=\"resume-degree-org text-muted\">人工智能与计算机学院</div>\n" +
+                    "</li>\n" +
+                    "<li>\n" +
+                    "<div class=\"resume-degree font-weight-bold\">客座硕士 · 2024 - 2023</div>\n" +
+                    "<div class=\"resume-degree-org fw-bold fs-6\">中国科学院计算技术研究所</div>\n" +
+                    "<div class=\"resume-degree-org text-muted\">处理器芯片全国重点实验室</div>\n" +
+                    "</li>",
+                "awa":
+                    "荣誉",
+                "awards":
+                    "<li>\n" +
+                    "<div class=\"font-weight-bold fs-6\">本科期间</div>\n" +
+                    "</li>\n" +
+                    "<li class=\"mb-3\">\n" +
+                    "<div class=\"text-muted mb-1\">院级三好学生(2020 - 2022)</div>\n" +
+                    "<div class=\"text-muted\">优秀毕业生</div>\n" +
+                    "</li>\n" +
+                    "<li>\n" +
+                    "<div class=\"font-weight-bold fs-6\">硕士期间</div>\n" +
+                    "</li>\n" +
+                    "<li>\n" +
+                    "<div class=\"text-muted mb-1\">[队长]2024全国大学生计算机系统能力大赛编译系统挑战赛(毕昇杯)全国二等奖/冠军</div>\n" +
+                    "<div class=\"text-muted mb-1\">2024全国大学生计算机系统能力大赛智能计算创新设计赛(先导杯)全国三等奖</div>\n" +
+                    "<div class=\"text-muted mb-1\">院研究生会优秀干事</div>\n" +
+                    "<div class=\"text-muted mb-1\">2023-2024学年综合测评专业第一(1/72)</div>\n" +
+                    "<div class=\"text-muted mb-1\">2023-2024学年一等学业奖学金(前5%)</div>\n" +
+                    "<div class=\"text-muted mb-1\">2023-2024学年优秀研究生干部(院22人获奖)</div>\n" +
+                    "<div class=\"text-muted\">2023-2024学年“小米奖学金”(院11人获奖)</div>\n" +
+                    "</li>",
+                "lang":
+                    "语言",
+                "langs":
+                    "<li class=\"mb-1\">中文 <span class=\"text-muted\"></span></li>\n" +
+                    "<li class=\"mb-1\">英语 <span class=\"text-muted\">(专业、学术)</span></li>\n" +
+                    "<li class=\"mb-1\">四级: 601</li><li>六级: 550</li>",
+                "int":
+                    "兴趣",
+                "sw":
+                    "社会工作",
+                "sws":
+                    "<li class=\"mb-1\">人工智能23级专硕班长</li>\n" +
+                    "<li class=\"mb-1\">2023 - 2024院研究生会办公室干事</li>\n" +
+                    "<li class=\"mb-1\">2023 - 2024校研究生会办公室干事</li>\n" +
+                    "<li>2023 - 2024志愿服务70小时</li>",
+                "inter":
+                    "<li class=\"mb-1\">篮球(高中校队)</li>\n" +
+                    "<li class=\"mb-1\">羽毛球</li>\n" +
+                    "<li class=\"mb-1\">短跑(国三/高中冠军)</li>\n" +
+                    "<li>游泳</li>",
+                "we":"实习经历",
+                "proj": "工程项目",
+                "pp": "发表论文",
+                "ri": "研究兴趣"
+            }
+        }
+    }
+;
+
+// 初始化 i18n
+i18next.init({
+    lng: 'en',  // 默认语言
+    debug: true,
+    resources
+}, function (err, t) {
+    updateContent();
+});
+
+// 切换语言
+function changeLanguage(lang) {
+    i18next.changeLanguage(lang, updateContent);
+}
+
+// 更新页面文本
+function updateContent() {
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        el.innerHTML = i18next.t(el.getAttribute("data-i18n"));
+    });
+}
